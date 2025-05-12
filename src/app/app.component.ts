784fb0dc47b32def27ff172769cf768c6736c7ef
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-05-12 12:54:53 +0200

    add heading and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TipCalculatorComponent } from './tip_calculator/tip-calculator/tip-calculator.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TipCalculatorComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'tip-calculator-angular';
---
+++ src/app/tip_calculator/tip-calculator/tip-calculator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tip-calculator',
+  imports: [],
+  templateUrl: './tip-calculator.component.html',
+  styleUrl: './tip-calculator.component.css'
+})
+export class TipCalculatorComponent {
+
+}
